ff9a465c8655dba0099c33ef5d867431a209a883
Vyros17 <162827531+Vyros17@users.noreply.github.com>
2025-02-04 20:35:12 -0400

    Update weather_analizer.dart

--- console/lib/weather_analizer.dart
+++ console/lib/weather_analizer.dart
@@ -2,13 +2,14 @@ import "dart:math";
 void events(dynamic city) {
   Random rn = Random();
   if (city["eventos"] == null || (city["eventos"]["dias"] == 0)) {
+    city["eventos"]?["evento"] = "";
     if (rn.nextInt(11) == 7) {
-      if (city["clima"] == "frio") {
+      if (city["clima"] == "".endsWith("frio")) {
         city["eventos"] = {
           "dias": rn.nextInt(31),
           "evento": "Tormenta de nieve"
         };
-      } else if (city["clima"] == "Caluroso") {
+      } else if (city["clima"] == "".endsWith("caluroso")) {
         city["eventos"] = {"dias": rn.nextInt(31), "evento": "Ola de Calor"};
       } else {
         city["eventos"] = {
@@ -19,8 +20,6 @@ void events(dynamic city) {
     }
   } else if (city["eventos"]["dias"] > 0) {
     city["eventos"]["dias"]--;
-  } else if (city["eventos"]["dias"] == 0) {
-    city["eventos"]["evento"] == "";
   }
 }
 
